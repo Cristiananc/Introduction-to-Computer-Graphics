3331578b375d53e0ecc82f7d38f67cd294d7e7d2
cristiana <cristiananc@hotmail.com>
2019-11-04 23:23:42 -0300

    Adding flip function

--- A2Cristiana/JS/main.js
+++ A2Cristiana/JS/main.js
@@ -39,20 +39,20 @@ var r90 = "rotate(90, 200, 200)";
 var r180 = "rotate(180, 200, 200)";
 var r270 = "rotate(270, 200, 200)";
 var scaleHalf = "translate( 0 0) translate(800 0) scale( 0.5 ) translate( -800 0)"
+var flipH = "scale( -1 1 )"
 
-var r90deg600 =  "rotate(-90, 600, 200)";
-var r180deg600 =  "rotate(-180, 600, 200)";
-var r270deg600 =  "rotate(-270, 600, 200)";
+var r90deg600 =  "rotate(90, 400, 400)";
+var r180deg600 =  "rotate(180, 400, 400)";
+var r270deg600 =  "rotate(270, 400, 400)";
 
-function pattern4x4(tile, x, y, group, r1, r2, r3, id1, id2, id3, id4) {
+
+// Aplica um conjunto de regras de transformação para formar um padrão 4x4
+function pattern4x4(tile, group, r1, r2, r3, id1, id2, id3, id4) {
     clone(tile, 4, group)
     let n0 = document.getElementById(id1);
     let n1 = document.getElementById(id2);
     let n2 = document.getElementById(id3);
     let n3 = document.getElementById(id4);
-
-    n0.setAttributeNS(null, 'x', x);
-    n0.setAttributeNS(null, 'y', y);
 
     n1.setAttributeNS(null, 'transform', r1);
     n2.setAttributeNS(null, 'transform', r2);
@@ -61,21 +61,39 @@ function pattern4x4(tile, x, y, group, r1, r2, r3, id1, id2, id3, id4) {
 
 createGroups('g1');
 createUse("#g1");
-
 var g1 = document.getElementById('g1');
 
-pattern4x4(tile2, 0, 0, g1, r90, r180, r270, 'newT01', 'newT11', 'newT21', 'newT31');
+pattern4x4(tile2, g1, r90, r180, r270, 'newT01', 'newT11', 'newT21', 'newT31');
 
 // Faço uma transformação de escala e repito as rotações anteriores
 createGroups('g2');
 createUse("#g2");
 var g2 = document.getElementById('g2');
-
 clone(g1, 1, g2);
-g2.setAttributeNS(null, 'transform', scaleHalf);
 
 createGroups('g3');
 createUse("#g3");
 var g3 = document.getElementById('g3');
 
-pattern4x4(g2, 400, 0, g2, r90deg600, r180deg600, r270deg600, 'newT03','newT13','newT23','newT33');
+pattern4x4(g1, g3, r90deg600, r180deg600, r270deg600, 'newT03','newT13','newT23','newT33');
+g3.setAttributeNS(null, 'transform', scaleHalf);
+
+// Ref : https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+// Transformação flip aleatória
+function horizontalFlip (n){
+    for (i = 0; i < n; i++){
+        random = getRandomInt(0, 4);
+        aux = "newT" + random + 3;
+        let element = document.getElementById(aux);
+        element.setAttributeNS(null, 'transform', flipH);
+        console.log(element)
+    }
+}
+//horizontalFlip(1);
+
